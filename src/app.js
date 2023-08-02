ffd1334a0165c6d5509b4ea73eaaee7d29f6d7c0
jemensoli <128940851+jemensoli@users.noreply.github.com>
2023-08-02 02:22:09 +0000

    Version 1.0

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let arreglo_pronombre = ["Yo", "el", "Nosotros"];
+  let arreglo_verbo = ["corro", "alcanzo", "sonrio"];
+  let arreglo_adj = ["rapido", "alto", "feliz"];
+  let arreglo_sustantivo = ["bondad", "delicadeza", "sensatez"];
+  let count = 0;
+
+  //-variables auxiliares se declaran dentro de cada for para capturar el elemento de cada array/
+  for (let auxelemento_p of arreglo_pronombre) {
+    for (let auxelemento_v of arreglo_verbo) {
+      for (let auxelemento_ad of arreglo_adj) {
+        for (let auxelemento_s of arreglo_sustantivo) {
+          console.log(
+            auxelemento_p +
+              auxelemento_v +
+              auxelemento_ad +
+              auxelemento_s +
+              ".com"
+          );
+          count++;
+        }
+      }
+    }
+  }
+  //-SALIDA: PRONOMBRE+VERBO+ADJ+SUSTANT//  //write your code here
+  console.log("La cantidad de dominios generados es: " + count);
 };
